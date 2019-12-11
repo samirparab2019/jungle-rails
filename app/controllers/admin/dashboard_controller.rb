b44195a5b0d32702e156e45fdfa344625d43b01d
Samir Parab <samirparab@yahoo.com>
2019-12-11 20:59:45 +0000

    admin dashboard updated to display count of how many products and categories are in the database.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @product_count = Product.count
+
+    @category_count = Product.group(:category_id).count
   end
 end
