150b890ea197b0648fc3f6bdc1c648ec35f3c0c1
Samir Parab <samirparab@yahoo.com>
2019-12-17 20:16:58 +0000

    user specs login modified.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
   # app/controllers/sessions_controller.rb
 
   def create
-    user = User.find_by_email(params[:email])
+    
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
     # if user = User.authenticate_with_credentials(params[:email], params[:password])
     # If the user exists AND the password entered is correct.
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
-      if user && user.authenticate(params[:password])
+      
+      
       session[:user_id] = user.id
       redirect_to '/'
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to root_path
     else
       redirect_to '/signup'
     end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email.downcase.strip, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
   
